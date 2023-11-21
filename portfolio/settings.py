22ee8b092e5ec0fb8a542378b3cd2fea73eb9b03
Bruno <bruno.paternostro@gmail.com>
2023-11-21 16:29:14 -0300

    adding allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z1^o*$q=n0h6iuow09f=)c89(*lo!bo+nc2o@v9lvmlkudnmf+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
