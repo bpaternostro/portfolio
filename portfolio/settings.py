896d8411dbefff185f3caafb40a7fef4ac47a000
Bruno <bruno.paternostro@gmail.com>
2023-12-05 16:29:26 -0300

    fix: ssl

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -49,6 +49,24 @@ CSRF_TRUSTED_ORIGINS = [
 
 # settings.py
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+# settings.py
+SECURE_SSL_REDIRECT = True
+# settings.py
+
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+# settings.py
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+# settings.py
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 # Application definition
 
 INSTALLED_APPS = [
