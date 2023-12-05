10a119ba8907cd8c525960b41f982f7e0d4c116d
Bruno <bruno.paternostro@gmail.com>
2023-12-05 15:22:53 -0300

    fix: csrf

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,10 +39,10 @@ CORS_ALLOWED_ORIGINS = [
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost',
-    'http://127.0.0.1',
-    'http://0.0.0.0',
-    'http://195.35.42.23',
+    #'http://localhost',
+    #'http://127.0.0.1',
+    #'http://0.0.0.0',
+    #'http://195.35.42.23',
     'http://bpaternostro.site',
     'https://bpaternostro.site',
 ]
